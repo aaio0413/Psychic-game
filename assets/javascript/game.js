8ed602325d632c73c6cccf37a49267545c79dca3
AAIO1992 <takahiro04kawaguchi13@gmail.com>
2018-06-05 17:20:14 -0700

    Fixed javascript to make this game works. Still Trying to set attribute on the sentence when either you lose or win.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,9 @@ var loses = 0;
 var guesses = "";
 var guessNum = 10;
 
+window.onload = function() {
+    updateResults();
+}
 
 function generateChar() {
     var char = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -19,20 +22,24 @@ function updateResults() {
     document.querySelector("#Guesses-letters").innerHTML = guesses;
 }
 
+
 document.onkeyup = function(e) {
     var userGuesses = e.key;
     var computerGuesses = generateChar();
     var lettersDiv = document.getElementById("Guesses-letters");
     var guessDiv = document.getElementById("Guess-left")
     
-    while(!userGuesses == computerGuesses && !guessNum == 0) {
-        guesses += userGuesses.value + ", ";
+    if(userGuesses != computerGuesses && guessNum != 0) {
+        guesses += userGuesses + ", ";
         guessNum--;
+        console.log(guessNum)
+        console.log(userGuesses)
         updateResults();
     }
 
     if (userGuesses == computerGuesses) {
         document.querySelector("#lead").innerHTML = "Correct! The letter computer thinks is " + computerGuesses + " !!"
+        document.querySelector("#lead").setAttribute("style", "color: red, font-size: 25px")
         wins++;
         guesses="";
         guessNum = 10;
@@ -40,6 +47,7 @@ document.onkeyup = function(e) {
     }
     if (guessNum == 0) {
         document.querySelector("#lead").innerHTML = "Too Bad! The letter computer thinks is " + computerGuesses + ", you lost!!"
+        document.querySelector("#lead").setAttribute("style", "color: red, font-size: 25px")
         loses++;
         guesses="";
         guessNum = 10;
